65f04ea29c02f44f45987ace2a5889206e519ee3
Camilo Valdes <juan.valdes@newsoftlatam.com>
2025-10-26 14:26:28 -0500

    tipos de datos primitivos y por referencia en JAVA

--- src/platzi/play/Main.java
+++ src/platzi/play/Main.java
@@ -3,6 +3,8 @@ package platzi.play;
 import platzi.play.contenido.Pelicula;
 import platzi.play.plataforma.Usuario;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Scanner;
 
 public class Main{
@@ -12,17 +14,20 @@ public class Main{
       // Instanciamos objeto
       Pelicula pelicula = new Pelicula();
       pelicula.titulo = "El señor de los anillos";
-      pelicula.anioEstreno = 2001;
+      pelicula.fechaEstreno = LocalDate.of(2018,10,15);
       pelicula.genero = "Fantasía";
 
       // Generamos calificación
       pelicula.calificar(4.7);
 
+      // Imprimimos ficha tecnica
+      System.out.println(pelicula.obtenerFichaTecnica());
+
       // Instanciamos objeto de usuario
       Usuario usuario = new Usuario();
       usuario.nombre = "Juan";
       usuario.email = "cliente@sincorreo.com";
-
+      usuario.fechaRegistro = LocalDateTime.of(2025,12,11,17,15, 12);
       // Imprimimos
       usuario.ver(pelicula);
       //Scanner scanner = new Scanner(System.in);
