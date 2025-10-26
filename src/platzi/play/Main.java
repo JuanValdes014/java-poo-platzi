32575c0463043f08b28314111ec2ff5bfc2c93ea
Camilo Valdes <juan.valdes@newsoftlatam.com>
2025-10-26 14:13:56 -0500

    creación de clases y objetos en java

--- src/platzi/play/Main.java
+++ src/platzi/play/Main.java
@@ -1,21 +1,40 @@
 package platzi.play;
 
+import platzi.play.contenido.Pelicula;
+import platzi.play.plataforma.Usuario;
+
 import java.util.Scanner;
 
 public class Main{
   public static void main(String[] args){
       System.out.println("Platzi play! ");
 
-      Scanner scanner = new Scanner(System.in);
-      System.out.println("Cual es tu nombre?: ");
-      String nombre = scanner.nextLine();
+      // Instanciamos objeto
+      Pelicula pelicula = new Pelicula();
+      pelicula.titulo = "El señor de los anillos";
+      pelicula.anioEstreno = 2001;
+      pelicula.genero = "Fantasía";
 
-      System.out.println("Hola "  + nombre + ", esto es una prueba de platzi play");
+      // Generamos calificación
+      pelicula.calificar(4.7);
 
-      System.out.println("Cuantos años tienes?:");
+      // Instanciamos objeto de usuario
+      Usuario usuario = new Usuario();
+      usuario.nombre = "Juan";
+      usuario.email = "cliente@sincorreo.com";
 
-      Integer edad = scanner.nextInt();
+      // Imprimimos
+      usuario.ver(pelicula);
+      //Scanner scanner = new Scanner(System.in);
+      //System.out.println("Cual es tu nombre?: ");
+      //String nombre = scanner.nextLine();
 
-      System.out.println(nombre + " puedes ver contenido +" + edad);
+      //System.out.println("Hola "  + nombre + ", esto es una prueba de platzi play");
+
+      //System.out.println("Cuantos años tienes?:");
+
+      //Integer edad = scanner.nextInt();
+
+      //System.out.println(nombre + " puedes ver contenido +" + edad);
   }
 }
